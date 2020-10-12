7f7ed08c6843de96616d0aa21b3f6ec5d4183946
tintheturtle <tinhoang@bu.edu>
2020-10-12 16:51:40 -0400

    added profile

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"net"
 	"net/url"
 	"os"
-    "io/ioutil"
+	"io/ioutil"
 )
 
 func validation(help bool, url string) (int) {
@@ -29,33 +29,45 @@ func checkError(err error) {
 
 
 func main() {
+	// CLI Arguments processing 
 	helpPtr := flag.Bool("help", false, "Help Information")
 	urlPtr := flag.String("url", "", "URL to fetch")
+	profilePtr := flag.Int("profile", 1, "Number of requests to be made")
 	flag.Parse()
-
 	validation(*helpPtr, *urlPtr)
 
-	
+	// URL String Parsing
 	u, err := url.Parse(*urlPtr)
 	checkError(err)
+	var host = u.Hostname()
+	var port = u.Port()
 
-	conn, err := net.Dial("tcp", u.Hostname()+":"+u.Port())	
-	checkError(err)
-
-	var endpoint string
-	if u.EscapedPath() == "" {
+	// Get endpoint
+	var endpoint = u.EscapedPath()
+	if endpoint == "" {
 		endpoint = "/"
-	} else {
-		endpoint = u.EscapedPath()
+	} 
+
+	// Headers
+	var headers = "\r\nAccept: application/json\r\nConnection: close\r\n\r\n"
+
+	// Send requests however many times specified (default 1)
+	for i := 0; i < *profilePtr; i ++ {
+		// Connects to the address provided  
+		conn, err := net.Dial("tcp", host + ":" + port)	
+		checkError(err)
+
+		_, err = conn.Write([]byte("POST " + endpoint + " HTTP/1.0\r\nHost: " + host + headers ))
+		checkError(err)
+
+		result, err := ioutil.ReadAll(conn)
+		checkError(err)
+
+		fmt.Println(string(result))
 	}
 
-	_, err = conn.Write([]byte("POST " + endpoint + " HTTP/1.0\r\nHost: " + u.Hostname() + "\r\nAccept: application/json\r\nConnection: close\r\n\r\n"))
-	checkError(err)
+	
 
-	result, err := ioutil.ReadAll(conn)
-	checkError(err)
-
-    fmt.Println(string(result))
 
     os.Exit(0)
 
